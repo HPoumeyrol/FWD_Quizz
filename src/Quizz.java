8868b35e5a9002d38cbaf64976823c7a17567be0
HuguesPoumeyrol <HuguesPoumeyrol@BNPPF010.home>
2018-02-09 15:10:08 +0100

    First Version

--- src/Quizz.java
+++ src/Quizz.java
@@ -1,19 +1,52 @@
-/**
- * 
- */
+import java.util.Scanner;
 
-/**
- * @author HuguesPoumeyrol
- *
- */
-public class Quizz {
+public class Quizz{
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+  private static Scanner input = new Scanner(System.in);
 
-	}
+    public static void main(String[] args) {
+        int score= 0;
+
+        score= askQuestionStringAnswerAndGetScore("Nom du formateur","Jules", score);
+        score= askQuestionBooleanAnswerAndGetScore("Le formateur est'il brun",true, score);
+
+        System.out.print("Votre score final est " + score);
+
+    }
+
+    private static int askQuestionStringAnswerAndGetScore(String question, String goodAnswer, int previousScore)  {
+
+        System.out.println(question + "?");
+
+        String userAnswer= input.nextLine();
+        if(userAnswer.equals(goodAnswer)) {
+          System.out.println("Réponse correcte !");
+          return ++previousScore;
+        }
+        else
+        {
+          System.out.println("Réponse incorrecte ! La bonne réponse est " + goodAnswer + ".");
+          return previousScore;
+        }
+    }
+
+    private static int askQuestionBooleanAnswerAndGetScore(String question, boolean goodAnswer, int previousScore)  {
+
+        System.out.println(question + " (O/N) ?");
+
+        char userInput= input.next().charAt(0);
+        boolean userAnswer= (userInput == 'O' || userInput == 'o');
+
+        if(userAnswer = goodAnswer){
+          System.out.println("Reponse correcte !");
+          return ++previousScore;
+        }
+        else
+        {
+            System.out.println("Reponse incorrecte ! La bonne réponse " + (goodAnswer ? "Oui" : "Non"));
+            return previousScore;
+        }
+    }
+
 
 }
